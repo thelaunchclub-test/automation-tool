eba0e6890666ce8f355d96a7b350d76c2a43e5b4
Petchimuthu1520 <petchi.muthu@thelaunchclub.in>
2024-05-29 12:34:30 +0530

    Update : removed unwanted classes, changed the name of the path in the DirectoryInfo

--- commons/src/main/java/com/twozo/commons/util/DirectoryInfo.java
+++ commons/src/main/java/com/twozo/commons/util/DirectoryInfo.java
@@ -12,7 +12,7 @@ import java.io.File;
  * <p>
  * Example usage:
  * <pre>{@code
- * String seleniumDir = DirectoryInfo.getSeleniumDirectory();
+ * String seleniumDir = DirectoryInfo.getAutomationToolDirectory();
  * }</pre>
  * </p>
  *
@@ -22,12 +22,12 @@ import java.io.File;
 @Value
 public class DirectoryInfo {
 
+    private static final String separator = File.separator;
     private static final String USER_HOME = System.getProperty("user.home");
     private static final String USER_DIRECTORY = System.getProperty("user.dir");
-    private static final String SELENIUM = String.format("%s%sselenium", USER_HOME, File.separator);
-    private static final String FILE = String.format("%s%sfile", SELENIUM, File.separator);
-    private static final String JSON = String.format("%s%sjson", SELENIUM, File.separator);
-    private static final String CONF = String.format("%s%ssrc", USER_DIRECTORY, File.separator);
+    private static final String AUTOMATION_TOOL = String.format("%s%s%s", USER_HOME, separator, "automation tool");
+    private static final String FILE = String.format("%s%s%s", AUTOMATION_TOOL, separator,"file");
+    private static final String JSON = String.format("%s%s%s", AUTOMATION_TOOL, separator,"json");
 
     /**
      * Returns the path of the user's home directory.
@@ -39,16 +39,25 @@ public class DirectoryInfo {
     }
 
     /**
+     * Returns the path of the user's directory.
+     *
+     * @return the path of the user's directory
+     */
+    public static String getUserDirectory() {
+        return USER_DIRECTORY;
+    }
+
+    /**
      * Returns the path of the selenium directory located in the user's home directory.
      *
      * @return the path of the selenium directory
      */
-    public static String getSeleniumDirectory() {
-        return SELENIUM;
+    public static String getAutomationToolDirectory() {
+        return AUTOMATION_TOOL;
     }
 
     /**
-     * Returns the path of the file directory located in the selenium directory.
+     * Returns the path of the file directory located in the automation tool directory.
      *
      * @return the path of the file directory
      */
@@ -57,21 +66,11 @@ public class DirectoryInfo {
     }
 
     /**
-     * Returns the path of the JSON directory located in the selenium directory.
+     * Returns the path of the JSON directory located in the automation tool directory.
      *
      * @return the path of the JSON directory
      */
     public static String getJsonDirectory() {
         return JSON;
     }
-
-    /**
-     * Returns the path of the conf directory located in the selenium directory.
-     *
-     * @return the path of the conf directory
-     */
-    public static String getConfDirectory() {
-        return CONF;
-    }
-
 }
